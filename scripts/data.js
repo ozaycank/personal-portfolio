22ac4edfd2230b5f08b1932d66204ddd9cdff4a1
Özay Can Kırlı <ozaycank97@gmail.com>
2026-04-28 12:04:53 +0300

    Html , CSS, assets data added

---
+++ scripts/data.js
@@ -0,0 +1,74 @@
+// scripts/data.js
+
+export const experienceData = [
+  {
+    id: 1,
+    company: "HitaTek",
+    role: "Stajyer",
+    duration: "Eyl 2021 - Eki 2021",
+    description:
+      "Yedekleme ve veri kurtarma çözümleri üzerine demo projeler geliştirerek, farklı felaket senaryoları kapsamında sistem sürekliliği ve veri güvenliği üzerine çalışmalar gerçekleştirdim.",
+    technologies: [
+      "Veeam Backup and Replication",
+      "MongoDB",
+      "Rsync",
+      "Linux Process Management",
+    ],
+  },
+  {
+    id: 2,
+    company: "Intertech",
+    role: "Full Stack Developer Stajyer",
+    duration: "Ağu 2022 - Eyl 2022",
+    description:
+      "Agile Scrum metodolojisi kapsamında, Blockchain ve React kullanarak sanal cüzdan uygulamasının geliştirilmesinde görev aldım",
+    technologies: ["Blockchain", "Solidty", "React", "Typescript", "Figma"],
+  },
+  {
+    id: 3,
+    company: "BSH Turkey",
+    role: "Solution Architect Stajyer, Full Stack Geliştirici",
+    duration: "Eyl 2022 - Eyl 2024",
+    description:
+      "Solution Architect Stajyeri olarak görev aldığım süreçte, proje bazlı mimari tasarım, teknik yönetim ve sistem entegrasyonu çalışmalarında aktif rol aldım. .NET ve C# teknolojileri ile yeni projeler geliştirdim ve mevcut uygulamalarda hata analizi ile sorun giderme süreçlerini yönettim. ",
+    technologies: [
+      "Microsoft CRM",
+      "HTML5",
+      "Query Studio",
+      "Log Parser",
+      "C#",
+      "ASP.NET",
+      "RDP",
+      "CSS",
+      "Microsoft SQL Server",
+      "DataDog",
+      "SOAP",
+      "IIS",
+    ],
+  },
+  {
+    id: 4,
+    company: "Güzel Mimarlık - Mühendislik",
+    role: "Bilişim Uzmanı",
+    duration: "Haz 2025 - Devam Ediyor",
+    description:
+      "Mimarlık, mühendislik ve proje tasarım yazılımları üzerinde performans iyileştirme, süreç optimizasyonu ve yeni özellik geliştirme çalışmalarında aktif rol almaktayım.",
+    technologies: ["AutoCAD", "SketchUp"],
+  },
+];
+
+export const skillsData = [
+  { name: "JavaScript (ES6+)", level: "90%" },
+  { name: "React", level: "85%" },
+  { name: "TypeScript", level: "80%" },
+  { name: "Node.js", level: "80%" },
+
+  { name: "C#", level: "70%" },
+  { name: "ASP.NET", level: "70%" },
+
+  { name: "MongoDB", level: "75%" },
+  { name: "Microsoft SQL Server", level: "80%" },
+
+  { name: "Linux Process Management", level: "75%" },
+  { name: "IIS", level: "75%" },
+];
